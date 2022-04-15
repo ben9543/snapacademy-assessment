c31b5493abc7b136b88e2e551af7f699c0ac7319
Bundy-Mundi <bennie.ben095@gmail.com>
2022-04-15 00:25:34 -0700

    Added footer

--- static/js/scrolls.js
+++ static/js/scrolls.js
@@ -1,43 +1,69 @@
 const allSections = document.getElementsByTagName("section");
 const allNavItems = document.getElementsByClassName("nav_item");
 const nav = document.getElementById("navigator");
-const NAVGAP = 30;
+const NAVGAP = 20;
+
 var lastNavPos = window.pageYOffset || document.documentElement.scrollTop;
+var clickedMenu = null;
+var currentSection = null;
+var currentSectionYOffset = null;
 
 // Add click event to all nav items
 Array.from(allNavItems).forEach(item => {
     item.addEventListener("click", function(){
-        nav.style.opacity="1";
-    })
+        // The main difference between children and childNodes property 
+        // is that children work upon elements and childNodes on nodes 
+        // including non-element nodes like text and comment nodes.
+        clickedMenu = item.children[0].getAttribute("href").substring(1);
+    });
 });
 
 // Navbar scorll event
 document.addEventListener("scroll", function(){
     var newNavPos = window.pageYOffset || document.documentElement.scrollTop;
-    if(newNavPos===0){
-        
-        // Styling ...
-        nav.style.opacity = "1";
-
-        // Update
+    
+    if (clickedMenu){
+        currentSection = document.getElementById(clickedMenu);
+        currentSectionYOffset = currentSection.getBoundingClientRect().top;
         lastNavPos = newNavPos;
-    }
-    else if (lastNavPos > newNavPos){ // Whenever user upscrolls, make nav visible
-
-        // Styling ...
-        nav.style.opacity = "1";
-
-        // Update
-        lastNavPos = newNavPos;
-
-    } else {
-        if (newNavPos-lastNavPos>NAVGAP){ // If the gap is bigger than NAVGAP, make nav visible
-            
+        if (currentSectionYOffset === 0){ // Only updates when currentSectionYOffset <= 0
+            clickedMenu = null;
+        }
+    }else{
+        if(newNavPos===0){ // Nav at the top
+    
             // Styling ...
-            nav.style.opacity = "0";
-
+            nav.style.transform = "translateY(0px)";
+            nav.style.opacity = "1";
+            
             // Update
             lastNavPos = newNavPos;
+        } else if (lastNavPos > newNavPos){ // Upscrolls
+            // Styling ...
+            nav.style.transform = "translateY(-150px)";
+            nav.style.opacity = "0";
+            
+            // Update
+            lastNavPos = newNavPos;
+    
+        } else {
+            if (newNavPos-lastNavPos>NAVGAP){ // Downscrolls
+                
+                // Styling ...
+                nav.style.transform = "translateY(0px)";
+                nav.style.opacity = "1";
+                
+                // Update
+                lastNavPos = newNavPos;
+            }
         }
     }
 });
+
+// Section event scroll
+Array.from(allSections).forEach(sec => {
+    console.log(sec)
+    sec.addEventListener("scroll", function(){
+        console.log(sec.getBoundingClientRect().top);
+    });
+})
