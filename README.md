3c7e1c45c123faaa8ae2a66ba51c1e2567810e85
Bundy-Mundi <bennie.ben095@gmail.com>
2022-04-16 12:05:12 -0700

    Finished gallery

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
     1. develop a single-page website for your favorite small business in LA that does not already have its own website and 
     2. deploy it online. Please make sure to include a note on your website that it is an unofficial webpage for the small business.
 
+- [Yelp](https://www.yelp.com/biz/tbaar-cerritos)
+
 - If you're experienced with web dev, here are some features we'd like to see. Definitely don't try to implement them all! Pick 1-3 you're excited about and implement those. Again, you are allowed to use a template but we’ll be looking for your contributions beyond the template. 
 
     - [X] Use of JavaScript 
--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,22 @@
 var isOpen = false;
-const hamburger = document.getElementById("hamburger");
+const hamburgers = document.getElementsByClassName("hamburger");
 const menuList = document.getElementById("toggle-menu");
 const items = menuList.getElementsByTagName("li");
 function noScroll() {
     window.scrollTo(0, 0)
 }
-hamburger.addEventListener("click", ()=>{
-    menuList.style.transform = "translateX(0)";
-    menuList.style.opacity = "1";
-    isOpen = true;
+Array.from(hamburgers).forEach((ham)=>{
+    ham.addEventListener("click", ()=>{
+        menuList.style.transform = "translateX(0)";
+        menuList.style.opacity = "1";
+        isOpen = true;
+    });
 });
 Array.from(items).forEach(item=>{
     item.addEventListener("click", ()=>{
         menuList.style.transform = "translateX(-100%)";
         menuList.style.opacity = "0";
+        isOpen = false;
     });
 });
 if(isOpen){
